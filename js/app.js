70cc6f27f5abb31bf91c48f3639c6ab930e2ea24
Thomas Grandjean <thomasgrandjean45@gmail.com>
2024-10-03 15:29:27 +0200

    documentation app.js

--- js/app.js
+++ js/app.js
@@ -1,32 +1,22 @@
 // ====ANIMATIONS====
 
-document.addEventListener('DOMContentLoaded', () => {
-    // Ajoute un écouteur d'événement pour exécuter le code lorsque le DOM est complètement chargé
-    const observeElements = (selector, threshold) => {
-        // Sélectionne tous les éléments correspondant au sélecteur donné
+document.addEventListener('DOMContentLoaded', () => {                       // ajouter un écouteur d'événement pour exécuter le code lorsque le DOM est complètement chargé
+    const observeElements = (selector, threshold) => {                       // sélectionner tous les éléments correspondant au sélecteur donné
         const elements = document.querySelectorAll(selector);
         
-        // Crée une instance de IntersectionObserver
-        const observer = new IntersectionObserver((entries, observer) => {
-            // Parcourt toutes les entrées observées
-            entries.forEach(entry => {
-                // Vérifie si l'élément est visible dans le viewport
-                if (entry.isIntersecting) {
-                    // Ajoute la classe 'visible' à l'élément
-                    entry.target.classList.add('visible');
-                    // Arrête d'observer l'élément une fois l'animation déclenchée
-                    observer.unobserve(entry.target);
+        const observer = new IntersectionObserver((entries, observer) => {          // créér une instance de IntersectionObserver
+            entries.forEach(entry => {                                               // parcourir toutes les entrées observées
+                if (entry.isIntersecting) {                                         // vérifier si l'élément est visible dans le viewport
+                    entry.target.classList.add('visible');                           // ajouter la classe 'visible' à l'élément
+                    observer.unobserve(entry.target);                                // arrêter d'observer l'élément une fois l'animation déclenchée
                 }
             });
-        }, { threshold }); // Définit le seuil de visibilité pour déclencher l'observation
+        }, { threshold });                                                          // définir le seuil de visibilité pour déclencher l'observation
 
-        // Observe chaque élément sélectionné
-        elements.forEach(element => observer.observe(element));
+        elements.forEach(element => observer.observe(element));                      // observer chaque élément sélectionné
     };
 
-    //selecteurs cibles
-
-    observeElements('.project', 0.8);
+    observeElements('.project', 0.8);                       //selecteurs cibles
     observeElements('.soft-skill-icon', 0.5);
     observeElements('#subtitle', 0.5);
     observeElements('#soft-skill-icon-1', 0.8);
